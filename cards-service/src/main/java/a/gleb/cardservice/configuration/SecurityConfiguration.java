4295dbf5f9776fb72161e4c039039129ad34a180
Gleb <a.gleb@yahoo.com>
2023-11-23 19:49:38 +0500

    - Initial commit
    Signed-off-by: Gleb <a.gleb@yahoo.com>

--- cards-service/src/main/java/a/gleb/cardservice/configuration/SecurityConfiguration.java
+++
@@ -1,7 +0,0 @@
-package a.gleb.cardservice.configuration;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class SecurityConfiguration {
-}
--- cards-service/src/main/java/a/gleb/cardservice/db/repository/CardRepository.java
+++
@@ -1,9 +0,0 @@
-package a.gleb.cardservice.db.repository;
-
-import a.gleb.cardservice.db.entity.CardEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface CardRepository extends JpaRepository<CardEntity, UUID> {
-}
